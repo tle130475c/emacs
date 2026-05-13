36d807b9ccdbfef8d0c347dc3f7bdb37f1424e39
tle130475c <tle130475c@gmail.com>
2026-05-13 22:00:44 +0700

    use misterioso theme

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(adwaita))
+ '(custom-enabled-themes '(misterioso))
  '(delete-selection-mode t)
  '(indent-tabs-mode nil)
  '(menu-bar-mode nil)
